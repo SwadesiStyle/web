7aece3292c63e974b94b05b628722bb356984bab
Swapnil Singh <swap@Swapnils-MacBook-Air.local>
2024-09-07 15:57:54 +0530

    Removes twitter stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,17 @@
 import { CartProvider } from 'components/cart/cart-context';
 import { Navbar } from 'components/layout/navbar';
-import { WelcomeToast } from 'components/welcome-toast';
-import { GeistSans } from 'geist/font/sans';
+// import { GeistSans } from 'geist/font/sans';
 import { getCart } from 'lib/shopify';
-import { ensureStartsWith } from 'lib/utils';
 import { cookies } from 'next/headers';
 import { ReactNode } from 'react';
-import { Toaster } from 'sonner';
 import './globals.css';
 
 const { TWITTER_CREATOR, TWITTER_SITE, SITE_NAME } = process.env;
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : 'http://localhost:3000';
-const twitterCreator = TWITTER_CREATOR ? ensureStartsWith(TWITTER_CREATOR, '@') : undefined;
-const twitterSite = TWITTER_SITE ? ensureStartsWith(TWITTER_SITE, 'https://') : undefined;
+// const twitterCreator = TWITTER_CREATOR ? ensureStartsWith(TWITTER_CREATOR, '@') : undefined;
+// const twitterSite = TWITTER_SITE ? ensureStartsWith(TWITTER_SITE, 'https://') : undefined;
 
 export const metadata = {
   metadataBase: new URL(baseUrl),
@@ -26,14 +23,14 @@ export const metadata = {
     follow: true,
     index: true
   },
-  ...(twitterCreator &&
-    twitterSite && {
-      twitter: {
-        card: 'summary_large_image',
-        creator: twitterCreator,
-        site: twitterSite
-      }
-    })
+  // ...(twitterCreator &&
+  //   twitterSite && {
+  //     twitter: {
+  //       card: 'summary_large_image',
+  //       creator: twitterCreator,
+  //       site: twitterSite
+  //     }
+  //   })
 };
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
@@ -42,14 +39,14 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
   const cart = getCart(cartId);
 
   return (
-    <html lang="en" className={GeistSans.variable}>
+    <html lang="en">
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
         <CartProvider cartPromise={cart}>
           <Navbar />
           <main>
             {children}
-            <Toaster closeButton />
-            <WelcomeToast />
+            {/* <Toaster closeButton /> */}
+            {/* <WelcomeToast /> */}
           </main>
         </CartProvider>
       </body>
