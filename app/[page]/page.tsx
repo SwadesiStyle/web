cc1c61f742120d33d21f40d860f021d115576189
Swapnil Singh <mail@swapnilsingh.me>
2024-10-06 15:57:55 +0530

    Removes dependency

--- app/[page]/page.tsx
+++ app/[page]/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 
-import Prose from 'components/prose';
-import { getPage } from 'lib/shopify';
+// import { getPage } from 'lib/shopify';
 import { notFound } from 'next/navigation';
 
 export async function generateMetadata({
@@ -9,37 +8,37 @@ export async function generateMetadata({
 }: {
   params: { page: string };
 }): Promise<Metadata> {
-  const page = await getPage(params.page);
+  // const page = await getPage(params.page);
 
-  if (!page) return notFound();
+  // if (!page) return notFound();
 
   return {
-    title: page.seo?.title || page.title,
-    description: page.seo?.description || page.bodySummary,
-    openGraph: {
-      publishedTime: page.createdAt,
-      modifiedTime: page.updatedAt,
-      type: 'article'
-    }
+    title: 'Swadesi',//page.seo?.title || page.title,
+    // description: page.seo?.description || page.bodySummary,
+    // openGraph: {
+      // publishedTime: page.createdAt,
+      // modifiedTime: page.updatedAt,
+      // type: 'article'
+    // }
   };
 }
 
 export default async function Page({ params }: { params: { page: string } }) {
-  const page = await getPage(params.page);
+  // const page = await getPage(params.page);
 
   if (!page) return notFound();
 
   return (
     <>
-      <h1 className="mb-8 text-5xl font-bold">{page.title}</h1>
-      <Prose className="mb-8" html={page.body as string} />
-      <p className="text-sm italic">
+      {/* <h1 className="mb-8 text-5xl font-bold">{page.title}</h1> */}
+      {/* <Prose className="mb-8" html={page.body as string} /> */}
+      {/* <p className="text-sm italic">
         {`This document was last updated on ${new Intl.DateTimeFormat(undefined, {
           year: 'numeric',
           month: 'long',
           day: 'numeric'
         }).format(new Date(page.updatedAt))}.`}
-      </p>
+      </p> */}
     </>
   );
 }
