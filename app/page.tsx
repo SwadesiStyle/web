688b80321a6008692da09e95ea1dfd7ef607b2f1
Swapnil Singh <mail@swapnilsingh.me>
2024-10-08 11:43:25 +0530

    Fixes build

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
+"use client";
 // import { Carousel } from 'components/carousel';
 // import { ThreeItemGrid } from 'components/grid/three-items';
 // import Footer from 'components/layout/footer';
 
-export const metadata = {
-  description: 'Swadesi is a community of creators, artists and conscious consumers united by shared ideas of sustainability, simplicity and thoughtful fashion',
-  openGraph: {
-    type: 'website'
-  }
-};
+// export const metadata = {
+//   description: 'Swadesi is a community of creators, artists and conscious consumers united by shared ideas of sustainability, simplicity and thoughtful fashion',
+//   openGraph: {
+//     type: 'website'
+//   }
+// };
 
 export default function HomePage() {
   return (
